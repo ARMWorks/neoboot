505bafd8b7727fdd1a8cb12f9be6c0ba2c79808d
Jeff Kent <jeff@jkent.net>
2019-10-27 04:11:39 -0500

    AArch32->AArch64 switch code, fixed mksunxiboot.py to prepend header

--- tools/mksunxiboot.py
+++ tools/mksunxiboot.py
@@ -4,6 +4,7 @@ import os
 import struct
 import sys
 
+
 HEADER_LEN = 96
 BLOCK_SIZE = 512
 MAX_SIZE = 32768
@@ -13,16 +14,24 @@ def gen_checksum(code):
     sum = 0
     for i in range(0, len(code), 4):
         sum += struct.unpack_from('<I', code, i)[0]
-    return sum & 0xFFFFFFFF
+        sum &= 0xFFFFFFFF
+    return sum
 
-with open(sys.argv[1], 'r+b') as f:
+with open(sys.argv[1], 'rb') as f:
+    stage1 = f.read(MAX_SIZE - HEADER_LEN)
+    stage1 += b'\0' * (BLOCK_SIZE + HEADER_LEN - ((len(stage1) + HEADER_LEN) % BLOCK_SIZE))
+    stage2 = f.read()
+
+with open(sys.argv[1], 'wb') as f:
+    instruction = 0xEA000000 | ((HEADER_LEN // 4 - 2) & 0x00FFFFFF)
+
     header = bytearray(HEADER_LEN)
-    header[:4] = f.read(4)
-    f.seek(HEADER_LEN)
-    code = f.read(MAX_SIZE - HEADER_LEN)
-    code += b'\0' * (BLOCK_SIZE - (len(code) + HEADER_LEN) % BLOCK_SIZE)
-    checksum = gen_checksum(header + code)
-    struct.pack_into('<8sII3sB', header, 4, b'eGON.BT0', checksum,
-        len(header + code), b'SPL', SPL_VERSION)
-    f.seek(0)
+    struct.pack_into('<I8sII3sB', header, 0, instruction, b'eGON.BT0', 0,
+        len(header) + len(stage1), b'SPL', SPL_VERSION)
+
+    checksum = gen_checksum(header + stage1)
+    struct.pack_into('<I', header, 12, checksum)
+
     f.write(header)
+    f.write(stage1)
+    f.write(stage2)
